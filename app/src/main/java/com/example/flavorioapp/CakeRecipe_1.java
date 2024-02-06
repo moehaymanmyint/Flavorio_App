d2c81bc176ca8919ab8477837dc062237a086b61
Razia003 <nssthwe003@gmail.com>
2024-02-06 19:08:53 +0630

    Added 4 interfaces

--- app/src/main/java/com/example/flavorioapp/CakeRecipe_1.java
+++ app/src/main/java/com/example/flavorioapp/CakeRecipe_1.java
@@ -8,6 +8,7 @@ import android.webkit.WebView;
 public class CakeRecipe_1 extends AppCompatActivity {
 
     WebView webView;
+    WebView webView1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +17,8 @@ public class CakeRecipe_1 extends AppCompatActivity {
 
         webView = findViewById(R.id.webview_chocolate);
         webView.loadUrl("file:///android_asset/chocolate_ingredients.html");
+
+        webView1 = findViewById(R.id.webview_chocorecipe);
+        webView1.loadUrl("file:///android_asset/chocolate_recipe.html");
     }
 }

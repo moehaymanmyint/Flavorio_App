ba840e65fbe1a5598c276988a1255c90e459f68c
Razia003 <nssthwe003@gmail.com>
2024-02-06 14:23:56 +0630

    Added web view

--- app/src/main/java/com/example/flavorioapp/RecipeInfo_1.java
+++ app/src/main/java/com/example/flavorioapp/RecipeInfo_1.java
@@ -3,12 +3,17 @@ package com.example.flavorioapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.webkit.WebView;
 
 public class RecipeInfo_1 extends AppCompatActivity {
+    WebView webView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipe_info1);
+
+        webView = findViewById(R.id.webView);
+        webView.loadUrl("file:///android_asset/recipeinfo1.html");
     }
 }

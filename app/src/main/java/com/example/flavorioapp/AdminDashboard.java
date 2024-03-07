b10628da23c3b13b4f3d8a1a1a626f238c66aa5b
moehaymanmyint <73158667+moehaymanmyint@users.noreply.github.com>
2024-03-07 08:24:42 +0630

    Designed Admin Dashboard

--- app/src/main/java/com/example/flavorioapp/AdminDashboard.java
+++ app/src/main/java/com/example/flavorioapp/AdminDashboard.java
@@ -2,13 +2,34 @@ package com.example.flavorioapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class AdminDashboard extends AppCompatActivity {
 
+    Button addBtn, viewBtn, updateBtn, deleteBtn, logoutBtn;
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.admin_dashboard);
+
+        addBtn = findViewById(R.id.addBtn);
+        viewBtn = findViewById(R.id.viewBtn);
+        updateBtn = findViewById(R.id.updateBtn);
+        deleteBtn = findViewById(R.id.deleteBtn);
+        logoutBtn = findViewById(R.id.logoutBtn);
+
+        logoutBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(AdminDashboard.this, SignIn.class);
+                startActivity(intent);
+            }
+        });
     }
 }
